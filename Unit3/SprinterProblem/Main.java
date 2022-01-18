7285ffb16b6bc54f9377fd744f4f0fdf4b61ae68
Eric Beaulne <eric.minecraft20@gmail.com>
2022-01-18 02:08:47 -0500

    eyyy

--- Unit3/SprinterProblem/Main.java
+++ Unit3/SprinterProblem/Main.java
@@ -19,42 +19,66 @@ public class Main {
 
         File dir = new File("Unit3\\SprinterProblem\\Files");
         File[] files = dir.listFiles();
-        
+
         // Print the files in the directory
         for (File file : files) {
+
             String filename = file.getName();
             String filepath = file.getPath();
+
             if (file.getName().endsWith(".in")) {
+
                 // Get the file with the answer.
                 String answerPath = filepath.substring(0, filepath.lastIndexOf('.')) + ".out";
+
+                // For Timing How Long it takes to solve.
                 long startTime = System.currentTimeMillis();
+
                 // Use the data in the .in file to solve the problem.
                 double solvedAnswer = Solver(filepath);
+
                 long endTime = System.currentTimeMillis();
+
                 // Read the answer from the .out file.
                 double answer = inputStream(answerPath).nextDouble();
+
                 // Compare the two answers.
-                System.out.println("Test case: " + filename.substring(0, filename.lastIndexOf('.')) + " | Calucated Answer: " + solvedAnswer + " | Correct Answer: " + answer + " | Same: " + (answer == solvedAnswer) + " |  Time: " + (endTime-startTime)+"ms");
+                System.out.println("Test case: " + filename.substring(0, filename.lastIndexOf('.'))
+                        + " | Calucated Answer: " + solvedAnswer + " | Correct Answer: " + answer + " | Same: "
+                        + (answer == solvedAnswer) + " |  Time: " + (endTime - startTime) + "ms");
             }
         }
     }
 
     public static double Solver(String inputFile) {
-        TreeMap<Integer, Integer> data = new TreeMap<Integer, Integer>(); // TreeMap to store the data.
-        Scanner in = inputStream(inputFile); // Get File
-        int n = in.nextInt(); // Take in the data
+        // TreeMap to store the data.
+        TreeMap<Integer, Integer> data = new TreeMap<Integer, Integer>();
+
+        // Get File
+        Scanner in = inputStream(inputFile);
+
+        // Take in the data
+        int n = in.nextInt();
         while (n-- > 0) {
             data.put(in.nextInt(), in.nextInt());
         }
+
+        // Iterate through the data and find the max value
         double max = 0.0;
+
         Integer[] keys = data.keySet().toArray(new Integer[0]);
-        for (int i = 1; i < data.size(); i++) {// Iterate through the data and find the max value
-            double distance = (double) Math.abs(data.get(keys[i]) - data.get(keys[i - 1])) / (double) (keys[i] - keys[i - 1]); // Equation for the distance | d = abs(y2-y1)/(x2-x1)
+
+        for (int i = 1; i < data.size(); i++) {
+            // Equation for the distance | d = abs(y2-y1)/(x2-x1)
+            double distance = (double) Math.abs(data.get(keys[i]) - data.get(keys[i - 1]))
+                    / (double) (keys[i] - keys[i - 1]);
             if (distance > max) {
                 max = distance;
             }
         }
-        return max; // Return Maximum distance between two points.
+
+        // Return Maximum distance between two points.
+        return max;
     }
 
     public static Scanner inputStream(String fileLoc) {
