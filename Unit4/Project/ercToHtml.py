427fb01ff3703ec7bfae2921309b24c0d7f75bde
Meaning <eric.minecraft20@gmail.com>
2022-01-28 20:59:01 -0500

    hi;hj

--- Unit4/Project/ercToHtml.py
+++ Unit4/Project/ercToHtml.py
@@ -1,4 +1,4 @@
 from erc import Erc
 ercData = open("Unit4\\Project\\file.erc", "r").read()
-ercd = Erc(ercData)
+ercd = Erc(ercData, "native")
 open("Unit4\\Project\\file.html", "w").write(ercd.html)
